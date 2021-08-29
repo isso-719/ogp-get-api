8a9f7da57492ba5ea41f2729669950ab3105e0e2
Kazuki Isogai <i@kazukiisogai.net>
2021-08-29 09:34:15 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+ruby '3.0.0'
+source 'https://rubygems.org'
+
+gem 'sinatra', '~> 2.1'
+gem 'sinatra-contrib', '~> 2.1'
+gem 'webrick', '~> 1.7'
+gem 'mechanize'
+
+group :development do
+  gem 'debase'
+  gem 'fastri'
+  gem 'rcodetools'
+  gem 'rubocop'
+  gem 'ruby-debug-ide'
+end
---
+++ app.rb
@@ -0,0 +1,85 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'json'
+
+get '/' do
+	if params[:url]
+		agent = Mechanize.new
+		page = agent.get(params[:url])
+
+		result = {}
+
+		begin
+			result[:title] = page.title
+		rescue => exception
+			result[:title] = nil
+		end
+
+		begin
+			result[:description] = page.description
+		rescue => exception
+			result[:description] = nil
+		end
+
+		begin
+			result[:keywords] = page.keywords
+		rescue => exception
+			result[:keywords] = nil
+		end
+
+		begin
+			result[:ogp_title] = page.at('meta[property="og:title"]')[:content]
+		rescue => exception
+			result[:ogp_title] = nil
+		end
+
+		begin
+			result[:ogp_description] = page.at('meta[property="og:description"]')[:content]
+		rescue => exception
+			result[:ogp_description] = nil
+		end
+
+		begin
+			result[:ogp_keywords] = page.at('meta[property="og:keywords"]')[:content]
+		rescue => exception
+			result[:ogp_keywords] = nil
+		end
+
+		begin
+			result[:ogp_image] = page.at('meta[property="og:image"]')[:content]
+		rescue => exception
+			result[:ogp_image] = nil
+		end
+
+		begin
+			result[:ogp_url] = page.at('meta[property="og:url"]')[:content]
+		rescue => exception
+			result[:ogp_url] = nil
+		end
+
+		begin
+			result[:ogp_type] = page.at('meta[property="og:type"]')[:content]
+		rescue => exception
+			result[:ogp_type] = nil
+		end
+
+		begin
+			result[:ogp_site_name] = page.at('meta[property="og:site_name"]')[:content]
+		rescue => exception
+			result[:ogp_site_name] = nil
+		end
+
+		begin
+			result[:ogp_locale] = page.at('meta[property="og:locale"]')[:content]
+		rescue => exception
+			result[:ogp_locale] = nil
+		end
+
+		content_type :json
+		result.to_json
+	
+	else
+		erb :index
+	end
+end
